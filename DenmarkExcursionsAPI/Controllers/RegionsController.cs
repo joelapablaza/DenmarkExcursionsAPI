95438315d5c19ff0fa971b71b232bfcbd8975569
joelapablaza <joelapablaza@gmail.com>
2023-02-24 01:29:31 -0300

    CRUD finished

--- DenmarkExcursionsAPI/Controllers/RegionsController.cs
+++ DenmarkExcursionsAPI/Controllers/RegionsController.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using DenmarkExcursionsAPI.Models.Domain;
+using DenmarkExcursionsAPI.Models.DTO;
 using DenmarkExcursionsAPI.Repositories;
 using Microsoft.AspNetCore.Mvc;
 
@@ -19,7 +20,7 @@ namespace DenmarkExcursionsAPI.Controllers
         }
 
         [HttpGet]
-        public async Task<IActionResult> GetAllRegions()
+        public async Task<IActionResult> GetAllRegionsAsync()
         {
             var regions = await _regionRepository.GetAllAsync();
 
@@ -45,6 +46,117 @@ namespace DenmarkExcursionsAPI.Controllers
             var regionsDTO = _mapper.Map<List<Models.DTO.Region>>(regions);
 
             return Ok(regionsDTO);
-        } 
+        }
+
+        [HttpGet]
+        [Route("{id:guid}")]
+        [ActionName("GetRegionAsync")]
+        public async Task<IActionResult> GetRegionAsync(Guid id)
+        {
+            var region = await _regionRepository.GetAsync(id);
+
+            if (region == null)
+            {
+                return NotFound();
+            }
+
+            var regionDTO = _mapper.Map<Models.DTO.Region>(region);
+
+            return Ok(regionDTO);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddRegionAsync(AddRegionRequest addRegionRequest)
+        {
+            // Request(DTO) to Domain model
+            var region = new Models.Domain.Region()
+            {
+                Code = addRegionRequest.Code,
+                Name = addRegionRequest.Name,
+                Area = addRegionRequest.Area,
+                Lat = addRegionRequest.Lat,
+                Long = addRegionRequest.Long,
+                Population = addRegionRequest.Population,
+            };
+            // Passing details to Repository
+            region = await _regionRepository.AddAsync(region);
+
+            // Converting back to DTO to return it
+            var regionDTO = new Models.DTO.Region()
+            {
+                Id = region.Id,
+                Code = region.Code,
+                Name = region.Name,
+                Area = region.Area,
+                Lat = region.Lat,
+                Long = region.Long,
+                Population = region.Population,
+            };
+
+            return CreatedAtAction(nameof(GetRegionAsync), new { id = regionDTO.Id}, regionDTO);
+        }
+
+        [HttpDelete]
+        [Route("{id:guid}")]
+        public async Task<IActionResult> DeleteRegionAsync(Guid id)
+        {
+            // Delete region from database
+            var region = await _regionRepository.DeleteAsync(id);
+            // If null NotFound
+            if (region == null)
+            {
+                return NotFound();
+            }
+            // Convert response back to DTO
+            var regionDTO = new Models.DTO.Region()
+            {
+                Id = region.Id,
+                Code = region.Code,
+                Name = region.Name,
+                Area = region.Area,
+                Lat = region.Lat,
+                Long = region.Long,
+                Population = region.Population,
+            };
+            // return Ok response
+            return Ok(regionDTO);
+        }
+
+        [HttpPut]
+        [Route("{id:guid}")]
+        public async Task<IActionResult> UpdateRegionAsync([FromRoute] Guid id, [FromBody] UpdateRegionRequest updateRegionRequest)
+        {
+            // Convert DTO to Domain
+            var region = new Models.Domain.Region()
+            {
+                Code = updateRegionRequest.Code,
+                Name = updateRegionRequest.Name,
+                Area = updateRegionRequest.Area,
+                Lat = updateRegionRequest.Lat,
+                Long = updateRegionRequest.Long,
+                Population = updateRegionRequest.Population,
+            };
+            // Update Region using repository
+            var updatedRegion = await _regionRepository.UpdateAsync(id, region);
+            // If null then NotFound
+            if (updatedRegion == null)
+            {
+                return NotFound();
+            }
+            // Convert Domain to DTO
+            var regionDTO = new Models.DTO.Region()
+            {
+                Id = updatedRegion.Id,
+                Code = updatedRegion.Code,
+                Name = updatedRegion.Name,
+                Area = updatedRegion.Area,
+                Lat = updatedRegion.Lat,
+                Long = updatedRegion.Long,
+                Population = updatedRegion.Population,
+            };
+            // Return Ok response
+            return Ok(regionDTO);
+
+        }
     }
 }
--- DenmarkExcursionsAPI/Repositories/RegionRepository.cs
+++ DenmarkExcursionsAPI/Repositories/RegionRepository.cs
@@ -12,11 +12,58 @@ namespace DenmarkExcursionsAPI.Repositories
         {
             _denmarkExcursionsDbContext = denmarkExcursionsDbContext;
         }
+
+        // Get All Regions
         public async Task<IEnumerable<Region>> GetAllAsync()
         {
             return await _denmarkExcursionsDbContext.Regions.ToListAsync();
+        }
 
+        // Get One Region by Id
+        public async Task<Region> GetAsync(Guid id)
+        {
+            return await  _denmarkExcursionsDbContext.Regions.FirstOrDefaultAsync(x => x.Id == id);
+        }
 
+        // Create One Region
+        public async Task<Region> AddAsync(Region region)
+        {
+            region.Id = Guid.NewGuid();
+            await _denmarkExcursionsDbContext.AddAsync(region);
+            await _denmarkExcursionsDbContext.SaveChangesAsync();
+            return region;
+        }
+
+        public async Task<Region> DeleteAsync(Guid id)
+        {
+            var region = await _denmarkExcursionsDbContext.Regions.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (region != null) 
+            {
+                _denmarkExcursionsDbContext.Regions.Remove(region);
+                await _denmarkExcursionsDbContext.SaveChangesAsync();
+            }
+
+            return region;
+        }
+
+        public async Task<Region> UpdateAsync(Guid id, Region region)
+        {
+            var regionUpdate = await _denmarkExcursionsDbContext.Regions.FirstOrDefaultAsync(x => x.Id == id);
+        
+            if (regionUpdate != null)
+            {
+                regionUpdate.Code = region.Code;
+                regionUpdate.Name= region.Name;
+                regionUpdate.Area = region.Area;
+                regionUpdate.Long = region.Long;
+                regionUpdate.Lat = region.Lat;
+                regionUpdate.Population = region.Population;
+
+                await _denmarkExcursionsDbContext.SaveChangesAsync();
+            }
+
+            return regionUpdate;
         }
     }
 }
